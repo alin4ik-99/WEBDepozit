754ed46f8ab2f1e04fc7d935b9cc9ba87f59a2f3
alin4ik_kvlnk <alinkakovalenko98@gmail.com>
2023-10-23 12:56:35 +0200

    addrazorruntime

--- WebDepozit/Program.cs
+++ WebDepozit/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddDbContext<DataContext>(options =>
 });
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
 var app = builder.Build();
 
